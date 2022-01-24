9a0c41fa121464cd662bc32d610f90bdbe7195a3
taplinseth <taplinseth@gmail.com>
2022-01-24 12:11:10 -0600

    marsLife

--- main.js
+++ main.js
@@ -10,6 +10,36 @@ const jobTypes = {
 };
 
 // Your code will go here
+class CrewMember {
+  constructor(name, job, specialSkill, ship) {
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = ship;
+  }
+
+  enterShip(ship) {
+    this.ship = ship;
+    ship.crew.push(this);
+  }
+}
+
+class Ship {
+  constructor(name, type, ability) {
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
+  }
+
+  missionStatement() {
+    if (this.crew.length > 0) {
+      return this.ability
+    } else {
+      return "Can't perform a mission yet.";
+    }
+  }
+}
 
 
 
